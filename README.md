a68b4eebe3ab72e9b3466e450f0341a77b084d18
Akshay Adiga <akshay.adiga@linux.vnet.ibm.com>
2017-09-05 20:33:54 +0530

    Improve lsmod2config to take commandline params

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Hacks
 Contains general code/scripts that come handy for linux developers
+
+## lsmod2config
+Converts a given lsmod output to a set of linux `CONFIG_*` options
+Prerequisite : linux kernel source.
+
+`./lsmod2config <linux_kernel_source> <lsmod_output>`
+
+`Example : ./lsmod2config ~/linux lsmod `
--- lsmod2config.sh
+++ lsmod2config.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-LINUX_DIR=/path/to/kernel/source/
+# expects LINUX_DIR 
 
-for modules in `cat lsmod | awk '{print $1}'`
+if [ $# -ne 2 ] ; then
+	echo "Usage :"
+	echo "$0 <linux_kernel_source> <lsmod_output>"
+	exit
+fi
+
+if [ ! -d $1 ] ; then
+	echo "$1 directory does not exits"
+	exit
+fi
+
+if [ ! -f $2 ] ; then
+	echo "$2  does not exits"
+	exit
+fi
+for modules in `cat $2 | awk '{print $1}'`
 do
 	str="\b$modules\.o\b"
-	line=`grep -R --include=Makefile  $str  $LINUX_DIR`
+	line=`grep -R --include=Makefile  $str  $1`
 	echo $line | awk -F"[()]" '{print $2}'i 
 done
